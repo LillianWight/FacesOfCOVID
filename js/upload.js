a2007381e759942adf9f4068d9fbfbb92777d4da
LillianWight <lillian.wight@gmail.com>
2020-04-24 19:15:24 -0600

    Minor adjustments to navigation page, submission form

--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,10 @@
 $("form").on("change", ".file-upload-field", function(){ 
     $(this).parent(".file-upload-wrapper").attr("data-text",         $(this).val().replace(/.*(\/|\\)/, '') );
 });
+
+form = document.getElementByClassName('needs-validation');
+if(form.checkValidity() === true) {
+  //form validation succeeded
+} else {
+  //form validation failed
+}
