d75e6fa4201e3e14dae1e3548b8f11d639e03ee5
LillianWight <lillian.wight@gmail.com>
2020-05-19 17:41:53 -0600

    corrected an error in upload.js

--- js/upload.js
+++ js/upload.js
@@ -2,7 +2,7 @@ $("form").on("change", ".file-upload-field", function(){
     $(this).parent(".file-upload-wrapper").attr("data-text",         $(this).val().replace(/.*(\/|\\)/, '') );
 });
 
-form = document.getElementByClassName('needs-validation');
+form = document.getElementsByClassName('needs-validation');
 if(form.checkValidity() === true) {
   //form validation succeeded
 } else {
